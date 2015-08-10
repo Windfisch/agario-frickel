81849dd42598f3f7ab22bfb313bd9ac56da3830e
Florian Jung <flo@windfisch.org>
2015-08-11 01:56:25 +0200

    stuff

--- main.py
+++ main.py
@@ -24,11 +24,10 @@ except:
 
 # connect
 c.connect(addr,token)
-#c.send_facebook(
-#            'g2gDYQFtAAAAEKO6L3c8C8/eXtbtbVJDGU5tAAAAUvOo7JuWAVSczT5Aj0eo0CvpeU8ijGzKy/gXBVCxhP5UO+ERH0jWjAo9bU1V7dU0GmwFr+SnzqWohx3qvG8Fg8RHlL17/y9ifVWpYUdweuODb9c=')
+c.send_facebook(
+            'g2gDYQFtAAAAEKO6L3c8C8/eXtbtbVJDGU5tAAAAUvOo7JuWAVSczT5Aj0eo0CvpeU8ijGzKy/gXBVCxhP5UO+ERH0jWjAo9bU1V7dU0GmwFr+SnzqWohx3qvG8Fg8RHlL17/y9ifVWpYUdweuODb9c=')
 
 c.player.nick="so mal sehen"
-c.send_spectate()
 
 
 # initialize GUI
