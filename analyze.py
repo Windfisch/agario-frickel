fdb391eff010c424ad8cd680d49e1523946e769e
Florian Jung <flo@windfisch.org>
2015-08-25 23:11:17 +0200

    analyze.py

---
+++ analyze.py
@@ -0,0 +1,14 @@
+from stats import *
+import sys
+
+if len(sys.argv) >= 2:
+    files = sys.argv[1:]
+else:
+    files = ["stats.pickle"]
+
+
+s = Stats.load(files[0])
+for f in files[1:]:
+    s.merge(f)
+
+s.analyze_speed()
